50de149f7abc8286196cccddea816965ee13b34a
jack <jack@jack.jack>
2020-08-16 15:26:04 -0400

    Updates

---
+++ precache-manifest.1b39ac87fb1a7ac34c4e4dc321f8ccaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481dbb02bcebb15d65a454281b76cdd0",
+    "url": "/FirstReact/index.html"
+  },
+  {
+    "revision": "e525a53292c8b91faac9",
+    "url": "/FirstReact/static/css/main.aed936d6.chunk.css"
+  },
+  {
+    "revision": "e6a34e96ee91106a023a",
+    "url": "/FirstReact/static/js/2.45eb8a58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstReact/static/js/2.45eb8a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e525a53292c8b91faac9",
+    "url": "/FirstReact/static/js/main.1e05624f.chunk.js"
+  },
+  {
+    "revision": "528e627df3ca270d9e26",
+    "url": "/FirstReact/static/js/runtime-main.94c0c38c.js"
+  }
+]);
